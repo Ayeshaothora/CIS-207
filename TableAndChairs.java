695f1646048122a70487f8c3747534301e03d014
Ayeshaothora <161149990+Ayeshaothora@users.noreply.github.com>
2025-02-05 19:42:37 -0500

    Add files via upload

---
+++ TableAndChairs.java
@@ -0,0 +1,14 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        // Printing the table part
+        System.out.println("+---+---+---+---+---+");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("+---+---+---+---+---+");
+
+        // Printing the chairs
+        System.out.println("    |   |   |   |");
+        System.out.println("    |   |   |   |");
+        System.out.println("    +---+---+---+");
+    }
+}
